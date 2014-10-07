9ad7a73b75b6653d747b13f450870bc5ee3b51f6
Chintan Vora <cvora@center4mi.org>
2014-10-07 08:21:00 -0700

    Graph project

---
+++ Graphs/Construction/GraphBuildingInC.cpp
@@ -0,0 +1,17 @@
+/*
+ * GraphBuildingInC.cpp
+ *
+ *  Created on: Oct 6, 2014
+ *      Author: cvora
+ */
+
+/*
+ * Graph is a data structure that consists of following two components:
+	1. A finite set of vertices also called as nodes.
+	2. A finite set of ordered pair of the form (u, v) called as edge.
+	The pair is ordered because (u, v) is not same as (v, u) in case of directed graph(di-graph).
+	The pair of form (u, v) indicates that there is an edge from vertex u to vertex v.
+	The edges may contain weight/value/cost.
+ */
+
+
