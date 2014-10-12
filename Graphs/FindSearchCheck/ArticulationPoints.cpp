f14cf7b7289a9e1967e24979c588dca16639963d
Chintan Vora <cvora@center4mi.org>
2014-10-11 20:01:45 -0700

    articulation point - understood a bit more now ... http://www.eecs.wsu.edu/~holder/courses/CptS223/spr08/slides/graphapps.pdf

--- Graphs/FindSearchCheck/ArticulationPoints.cpp
+++ Graphs/FindSearchCheck/ArticulationPoints.cpp
@@ -96,6 +96,8 @@ void Graph::addEdge(int v, int w)
 // disc[] --> Stores discovery times of visited vertices
 // parent[] --> Stores parent vertices in DFS tree
 // ap[] --> Store articulation points
+// Low[u ] = lowest numbered vertex reachable from u using 0 or more spanning tree edges and then at most one back edge
+// http://www.eecs.wsu.edu/~holder/courses/CptS223/spr08/slides/graphapps.pdf
 void Graph::APUtil(int u, bool visited[], int disc[],
                                       int low[], int parent[], bool ap[])
 {
@@ -133,11 +135,14 @@ void Graph::APUtil(int u, bool visited[], int disc[],
             // u is an articulation point in following cases
 
             // (1) u is root of DFS tree and has two or more chilren.
-            if (parent[u] == NIL && children > 1)
+            if (parent[u] == NIL && children > 1){
+            	cout<<"Satisying rule 1 : "<<u<<endl;
                ap[u] = true;
+            }
 
             // (2) If u is not root and low value of one of its child is more
             // than discovery value of u.
+            // I.e., is there a child v of u that cannot reach a vertex visited before u?
             if (parent[u] != NIL && low[v] >= disc[u])
                ap[u] = true;
         }
@@ -183,32 +188,37 @@ int main()
 {
     // Create graphs given in above diagrams
     cout << "\nArticulation points in first graph \n";
-    Graph g1(5);
-    g1.addEdge(1, 0);
-    g1.addEdge(0, 2);
-    g1.addEdge(2, 1);
-    g1.addEdge(0, 3);
-    g1.addEdge(3, 4);
+    Graph g1(4);
+    g1.addEdge(1,0);
+    g1.addEdge(1,2);
+    g1.addEdge(2,3);
     g1.AP();
-
-    cout << "\nArticulation points in second graph \n";
-    Graph g2(4);
-    g2.addEdge(0, 1);
-    g2.addEdge(1, 2);
-    g2.addEdge(2, 3);
-    g2.AP();
-
-    cout << "\nArticulation points in third graph \n";
-    Graph g3(7);
-    g3.addEdge(0, 1);
-    g3.addEdge(1, 2);
-    g3.addEdge(2, 0);
-    g3.addEdge(1, 3);
-    g3.addEdge(1, 4);
-    g3.addEdge(1, 6);
-    g3.addEdge(3, 5);
-    g3.addEdge(4, 5);
-    g3.AP();
+//    Graph g1(5);
+//    g1.addEdge(1, 0);
+//    g1.addEdge(0, 2);
+//    g1.addEdge(2, 1);
+//    g1.addEdge(0, 3);
+//    g1.addEdge(3, 4);
+//    g1.AP();
+//
+//    cout << "\nArticulation points in second graph \n";
+//    Graph g2(4);
+//    g2.addEdge(0, 1);
+//    g2.addEdge(1, 2);
+//    g2.addEdge(2, 3);
+//    g2.AP();
+//
+//    cout << "\nArticulation points in third graph \n";
+//    Graph g3(7);
+//    g3.addEdge(0, 1);
+//    g3.addEdge(1, 2);
+//    g3.addEdge(2, 0);
+//    g3.addEdge(1, 3);
+//    g3.addEdge(1, 4);
+//    g3.addEdge(1, 6);
+//    g3.addEdge(3, 5);
+//    g3.addEdge(4, 5);
+//    g3.AP();
 
     return 0;
 }
