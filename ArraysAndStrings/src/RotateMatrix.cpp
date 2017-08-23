695afd905a4d160a446f0ca7dd7594b5619f83c5
Chintan Vora <cvora22@gmail.com>
2017-08-22 17:30:24 -0700

    Strings Arrays chapter

--- ArraysAndStrings/src/RotateMatrix.cpp
+++ ArraysAndStrings/src/RotateMatrix.cpp
@@ -14,7 +14,6 @@
 
 using namespace std;
 
-
 template <size_t size_x,size_t size_y>
 void rotateMatrixSol1(int (&matrix)[size_x][size_y]){
 	int n = size_x;
@@ -23,15 +22,15 @@ void rotateMatrixSol1(int (&matrix)[size_x][size_y]){
 		int last = n - 1 - layer;
 		for(int i=first;i<last;++i){
 			int offset = i - first;
-			//save top
+			//save left_top
 			int top = matrix[first][i];
-			//left -> top
+			//left_bottom -> left_top
 			matrix[first][i] = matrix[last-offset][first];
-			//bottom -> left
+			//right_bottom -> left_bottom
 			matrix[last-offset][first] = matrix[last][last-offset];
-			// right -> bottom
+			// right_top -> right_bottom
 			matrix[last][last-offset] = matrix[i][last];
-			// top -> right
+			// left_top -> right_top
 			matrix[i][last] = top;
 		}
 	}
